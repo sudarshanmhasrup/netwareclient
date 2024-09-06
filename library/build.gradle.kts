e5c85039b551a5499ef365664b1a6d37fe8f576e
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-09-06 19:23:09 +0530

    Setup the project

---
+++ library/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.kotlinJvm)
+}
+
+group = "com.github.sudarshanmhasrup"
+version = "0.0.1-alpha"
+
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "Netware Client"
+include("library")
+include("examples")
